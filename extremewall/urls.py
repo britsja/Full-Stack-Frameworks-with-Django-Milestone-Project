580843070c460ad9bdaa4d175fee359bdca027e1
Jan Brits <pcerservices@gmail.com>
2019-02-12 19:53:04 +0200

    Basic home page and nav

--- extremewall/urls.py
+++ extremewall/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from extremewall import views
 from home.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name='home')
 ]
 
