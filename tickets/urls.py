db52a4965ca40054d46608dd68c3fa0321b39fde
Jan Brits <pcerservices@gmail.com>
2019-02-14 15:33:35 +0200

    Added functionality to show individual tickets from list of tickets

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_open_tickets, name="show_open_tickets")
+    path('', views.show_open_tickets, name="show_open_tickets"),
+    path('show_ticket/<id>', views.show_ticket, name="show_ticket")
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Ticket
 
 def show_open_tickets(request):
-    return render(request, 'showtickets.html')
+    tickets = Ticket.objects.all()
+    return render(request, 'showtickets.html', {'tickets': tickets})
+
+def show_ticket(request, id):
+    ticket = get_object_or_404(Ticket, pk=id)
+    return render(request, "openticket.html",{'ticket': ticket})
