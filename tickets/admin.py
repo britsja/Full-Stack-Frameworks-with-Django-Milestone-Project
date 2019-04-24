bac60298303cec35ba640fafa1dc896ec3f2552e
Jan Brits <pcerservices@gmail.com>
2019-04-24 14:42:16 +0000

    Removed reference to unused db table in ticket admin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ticket_username, Ticket, Ticketcategory, Comments
+#from .models import Ticket_username, Ticket, Ticketcategory, Comments
+from .models import Ticket, Ticketcategory, Comments
 
-admin.site.register(Ticket_username)
+#admin.site.register(Ticket_username)
 admin.site.register(Ticket)
 admin.site.register(Ticketcategory)
 admin.site.register(Comments)
